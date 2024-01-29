b06c951b97064e4b155377c1f8f15f3222c0a9e7
Tomas Zeman <the.zeman@gmail.com>
2024-01-29 20:50:52 +0800

    black

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
+    <div className="App bg-black w-full h-full flex items-center text-white justify-center">
+      <h1 className="text-3xl font-bold">
       Hello world! This is meeee thomino.
     </h1>
     </div>
